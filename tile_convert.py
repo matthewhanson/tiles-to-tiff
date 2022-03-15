ceb4fefd32f6d3eff68e19be1004661b1ff180c6
Matthew Hanson <matt.a.hanson@gmail.com>
2022-03-15 15:56:14 -0400

    hack to work with TMS tiles

--- tile_convert.py
+++ tile_convert.py
@@ -9,12 +9,14 @@ def latlon_to_xyz(lat, lon, z):
     tile_count = pow(2, z)
     x = (lon + 180) / 360
     y = (1 - log(tan(radians(lat)) + sec(radians(lat))) / pi) / 2
-    return(tile_count*x, tile_count*y)
+    y_ = tile_count*y
+    y = (2 ** z) - y_ - 1
+    return(tile_count*x, y)
 
 
 def bbox_to_xyz(lon_min, lon_max, lat_min, lat_max, z):
-    x_min, y_max = latlon_to_xyz(lat_min, lon_min, z)
-    x_max, y_min = latlon_to_xyz(lat_max, lon_max, z)
+    x_min, y_min = latlon_to_xyz(lat_min, lon_min, z)
+    x_max, y_max = latlon_to_xyz(lat_max, lon_max, z)
     return(floor(x_min), floor(x_max),
            floor(y_min), floor(y_max))
 
@@ -26,11 +28,12 @@ def mercatorToLat(mercatorY):
 def y_to_lat_edges(y, z):
     tile_count = pow(2, z)
     unit = 1 / tile_count
+    y_ = (2 ** z) - y + 1
     relative_y1 = y * unit
     relative_y2 = relative_y1 + unit
     lat1 = mercatorToLat(pi * (1 - 2 * relative_y1))
     lat2 = mercatorToLat(pi * (1 - 2 * relative_y2))
-    return(lat1, lat2)
+    return(-lat2, -lat1)
 
 
 def x_to_lon_edges(x, z):
